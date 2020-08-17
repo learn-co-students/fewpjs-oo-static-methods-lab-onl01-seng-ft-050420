e1731ea70693c38a1e025af90ee7dfa53da76053
aaron-schuyler <asky28@gmail.com>
2020-08-17 14:39:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+  static sanitize(str) {
+    return str.replace(/[^0-9a-zA-Z '-]/g, '')
+  }
+  static titleize(title) {
+    const arr = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    function capitalizeWord(word, i) {
+      if (i === 0) return Formatter.capitalize(word)
+      else if (arr.includes(word)) return word
+      else return Formatter.capitalize(word)
+    }
+    return title.split(' ').map(capitalizeWord).join(' ')
+  }
 }
