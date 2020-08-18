5d7eb00313e324a891d779452529c38d55a4f4ff
qu1xs <kevinnealmurphy@gmail.com>
2020-08-18 13:52:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^0-9a-zA-Z '-]/g, '');
+  }
+
+  static titleize(sentence) {
+    const badWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    return sentence.split(' ').map((word, i) => {
+      if (i === 0) return this.capitalize(word);
+      else if (badWords.includes(word)) return word;
+      else return this.capitalize(word);
+    }).join(' ')
+  }
+
 }
