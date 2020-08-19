9f4fdbf828eaf45834818247fa65ed6865182637
leobr <leobrionesherrera@gmail.com>
2020-08-19 17:09:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(str) {
+    const noCap = ["a" , "an", "but", "of", "and", "for", "at", "by", "from", "the"];
+    const words = str.split(" ");
+    words[0] = this.capitalize(words[0]);
+    for (let i = 1; i < words.length; i++) {
+      const word = words[i];
+      if (!noCap.includes(word)) words[i] = this.capitalize(word);
+    }
+    return words.join(" ");
+  }
 }
