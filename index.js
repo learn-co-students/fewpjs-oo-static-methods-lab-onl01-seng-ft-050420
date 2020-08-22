687e5883eb0571da520bd15a6f23f12be8bf3f7f
Adamlatorraca <adam.latorraca@gmail.com>
2020-08-22 16:00:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string) {
+    return string.replace(/[^\w,' -]/g, '')
+  }
+  static titleize(string) {
+    let givenWords = string.split(" ")
+    let badWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let titleizedSentence = givenWords.map((word, index) => {
+      if (index === 0) {
+        return this.capitalize(word)
+      } else if (badWords.includes(word)) {
+        return word
+      } else {
+        return this.capitalize(word)
+      }
+    }).join(" ")
+    return titleizedSentence
+  }
 }
